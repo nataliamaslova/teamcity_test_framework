541d9e16f13fc001682d138f43cd263b5b94e573
nataliamaslova <maslova.nd@gmail.com>
2024-03-03 21:48:06 +0100

    [init] Test framework

---
+++ src/main/java/com/example/teamcity/api/models/Roles.java
@@ -0,0 +1,15 @@
+package com.example.teamcity.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Roles {
+    List<Role> role;
+}
