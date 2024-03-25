f830f16fe290ea081ed60f3a17adf5f65a5da781
nataliamaslova <maslova.nd@gmail.com>
2024-03-25 22:31:31 +0100

    api: create project / create build config covered with tests

--- src/test/java/com/example/teamcity/api/CreateProjectTest.java
+++ src/test/java/com/example/teamcity/api/CreateProjectTest.java
@@ -1,7 +1,10 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.requests.UncheckedRequests;
 import com.example.teamcity.api.requests.checked.CheckedProject;
+import com.example.teamcity.api.requests.checked.CheckedUser;
+import com.example.teamcity.api.requests.unchecked.UncheckedProject;
 import com.example.teamcity.api.spec.Specifications;
 import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
@@ -9,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class CreateProjectTest extends BaseApiTest {
     @Test
-    public void createProjectTest() {
+    public void projectCreatedForStandardPositiveCaseTest() {
         var testData = testDataStorage.addTestData();
 
         checkedWithSuperUser.getUserRequest().create(testData.getUser());
@@ -22,7 +25,7 @@ public class CreateProjectTest extends BaseApiTest {
     }
 
     @Test
-    public void unauthorizedUserShouldNotCreateProject() {
+    public void projectIsNotCreatedForUnauthorizedUserTest() {
         var testData = testDataStorage.addTestData();
 
         new UncheckedRequests(Specifications.getSpec().unauthSpec()).getProjectRequest()
@@ -37,4 +40,125 @@ public class CreateProjectTest extends BaseApiTest {
                         " 'count:1,id:" + testData.getProject().getId() + "'"));
   }
 
+    @Test
+    public void projectIsNotCreatedForInvalidParentIdTest() {
+        var testData = testDataStorage.addTestData();
+        var invalidParentId = "abc";
+
+        testData.getProject().setParentProject(Project.builder()
+                .locator(invalidParentId)
+                .build());
+
+        new CheckedUser(Specifications.getSpec().superUserSpec())
+                .create(testData.getUser());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_NOT_FOUND)
+                .body(Matchers.containsString("No project found by name or internal/external id '"
+                        + invalidParentId + "'."));
+    }
+    @Test
+    public void projectIsNotCreatedForTheSameIdTest() {
+        var testData = testDataStorage.addTestData();
+
+        new CheckedUser(Specifications.getSpec().superUserSpec())
+                .create(testData.getUser());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat().statusCode(HttpStatus.SC_OK);
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("DuplicateProjectNameException: Project with this name already exists: "
+                                + testData.getProject().getName()));
+    }
+
+    @Test
+    public void projectIsNotCreatedForEmptyNameTest() {
+        var testData = testDataStorage.addTestData();
+
+        testData.getProject().setName("");
+
+        checkedWithSuperUser.getUserRequest().create(testData.getUser());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("Project name cannot be empty"));
+    }
+
+    @Test
+    public void projectIsNotCreatedForEmptyIdTest() {
+        var testData = testDataStorage.addTestData();
+
+        testData.getProject().setId("");
+
+        checkedWithSuperUser.getUserRequest().create(testData.getUser());
+
+        new UncheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject())
+                .then().assertThat()
+                .statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString("Project ID must not be empty"));
+    }
+
+    @Test
+    public void projectCreatedFor4000SymbolsInNameTest() {
+        var testData = testDataStorage.addTestData();
+        var expectedProjectName = "1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol1000Symbol";
+
+        checkedWithSuperUser.getUserRequest().create(testData.getUser());
+
+        testData.getProject().setName(expectedProjectName);
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        softy.assertThat(project.getName()).isEqualTo(expectedProjectName);
+    }
+
+    @Test
+    public void projectCreatedForRussianPolandEncodingInNameTest() {
+        var testData = testDataStorage.addTestData();
+        var expectedProjectName = "новый проект Wrocław/Żielona Góra";
+
+        testData.getProject().setName(expectedProjectName);
+
+        checkedWithSuperUser.getUserRequest().create(testData.getUser());
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        softy.assertThat(project.getName()).isEqualTo(expectedProjectName);
+    }
+
+    @Test
+    public void projectCreatedForSpecialSymbolsInNameTest() {
+        var testData = testDataStorage.addTestData();
+        var expectedProjectName = "!@#$%^&*()~:,.;'|/?";
+
+        testData.getProject().setName(expectedProjectName);
+
+        checkedWithSuperUser.getUserRequest().create(testData.getUser());
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        softy.assertThat(project.getName()).isEqualTo(expectedProjectName);
+    }
 }
