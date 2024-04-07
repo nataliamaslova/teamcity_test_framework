113911ea1846bbe4d3aecaf654c4cb16a04d8b90
nataliamaslova <maslova.nd@gmail.com>
2024-04-07 17:10:25 +0200

    [hm-3] get TeamCity agent name firstly

--- src/test/java/com/example/teamcity/api/AuthorizeAgentTest.java
+++ src/test/java/com/example/teamcity/api/AuthorizeAgentTest.java
@@ -9,13 +9,24 @@ import org.testng.annotations.Test;
 public class AuthorizeAgentTest extends BaseApiTest {
     @Test
     public void authorizeAgent() {
-       RestAssured
+        String agentName = RestAssured
+                .given()
+                .spec(Specifications.getSpec().superUserSpec())
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .get("/app/rest/agents")
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().jsonPath().getString("agent.name");
+
+        agentName = (agentName.substring(1, agentName.length() - 1));
+
+        RestAssured
                 .given()
                 .spec(Specifications.getSpec().superUserSpec())
                 .contentType(ContentType.TEXT)
                 .accept(ContentType.TEXT)
                 .body("true")
-                .put("/app/rest/agents/ip_172.17.0.1/authorized")
+                .put("/app/rest/agents/" + agentName + "/authorized") //  ip_172.17.0.1
                 .then().assertThat().statusCode(HttpStatus.SC_OK);
     }
 }
