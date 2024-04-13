55d337b3cf5222020068058d960434a1c4734da5
nataliamaslova <maslova.nd@gmail.com>
2024-04-13 11:07:24 +0200

    [hm-4] filters for swagger coverage removed, allure and swagger commented

--- src/main/java/com/example/teamcity/api/requests/unchecked/UncheckedProject.java
+++ src/main/java/com/example/teamcity/api/requests/unchecked/UncheckedProject.java
@@ -2,7 +2,6 @@ package com.example.teamcity.api.requests.unchecked;
 
 import com.example.teamcity.api.requests.CrudInterface;
 import com.example.teamcity.api.requests.Request;
-import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -18,7 +17,7 @@ public class UncheckedProject extends Request implements CrudInterface {
     @Override
     public Response create(Object obj) {
         return given()
-                .filter(new SwaggerCoverageRestAssured())
+//                .filter(new SwaggerCoverageRestAssured())
                 .spec(spec)
                 .body(obj)
                 .post(PROJECT_ENDPOINT);
@@ -27,7 +26,7 @@ public class UncheckedProject extends Request implements CrudInterface {
     @Override
     public Response get(String id) {
         return given()
-                .filter(new SwaggerCoverageRestAssured())
+//                .filter(new SwaggerCoverageRestAssured())
                 .spec(spec)
                 .get(PROJECT_ENDPOINT + "/id:" + id);
     }
@@ -40,7 +39,7 @@ public class UncheckedProject extends Request implements CrudInterface {
     @Override
     public Response delete(String id) {
         return given()
-                .filter(new SwaggerCoverageRestAssured())
+//                .filter(new SwaggerCoverageRestAssured())
                 .spec(spec)
                 .delete(PROJECT_ENDPOINT + "/id:" + id);
     }
